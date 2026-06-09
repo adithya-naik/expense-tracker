904544d2c7a9c5a048fd9a339a24237417e91bbf
adithya-naik <jatothadithyanaik@gmail.com>
2026-06-09 22:41:00 +0530

    Setup frontend authentication pages and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  return <>Expense Tracker</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
